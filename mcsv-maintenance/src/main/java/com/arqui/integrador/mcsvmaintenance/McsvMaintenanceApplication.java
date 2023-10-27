5c01d428d4086565acfd0b892f27f1ea43e24e77
Jesu <jesusdiaz013@gmail.com>
2023-10-27 16:39:28 -0300

    starting maintenance micro service

---
+++ mcsv-maintenance/src/main/java/com/arqui/integrador/mcsvmaintenance/McsvMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.arqui.integrador.mcsvmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvMaintenanceApplication.class, args);
+	}
+
+}
