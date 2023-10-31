4636a9d8ad5b30455c72330a7094e0f71d177dd9
Jesu <jesusdiaz013@gmail.com>
2023-10-31 20:21:28 -0300

    push para

--- mcsv-maintenance/src/main/java/com/arqui/integrador/mcsvmaintenance/controller/IMaintenanceController.java
+++ mcsv-maintenance/src/main/java/com/arqui/integrador/mcsvmaintenance/controller/IMaintenanceController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -50,44 +49,6 @@ public interface IMaintenanceController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	void delete (@PathVariable(name = "id") Long id);
 
-	
 
-
-
-
-
-
-
-
-
-
-
-	// @GetMapping(value = "/user/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	// @ResponseBody
-	// @ResponseStatus(HttpStatus.OK)
-	// ResponseEntity<UserDto> getById(@PathVariable(name = "id") Long id);
-	
-	// @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	// @ResponseBody
-	// @ResponseStatus(HttpStatus.CREATED)
-	// ResponseEntity<UserDto> create(@Valid @RequestBody UserDto user);
-	
-	// @PutMapping(value = "/user/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	// @ResponseBody
-	// @ResponseStatus(HttpStatus.OK)
-	// ResponseEntity<UserDto> update(@PathVariable(name = "id") Long id, @Valid @RequestBody UserDto user);
-	
-	// @PutMapping(value = "/user/add-account/{user-id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	// @ResponseBody
-	// @ResponseStatus(HttpStatus.OK)
-	// ResponseEntity<UserDto> addAccount(@PathVariable(name = "user-id") Long userId, @Valid @RequestBody AccountDto account);
-	
-	// @DeleteMapping(value = "/user/{id}")
-	// @ResponseStatus(HttpStatus.NO_CONTENT)
-	// void delete(@PathVariable(name = "id") Long id);
-	
-	// @DeleteMapping(value = "/user/delete-account/{user-id}")
-	// @ResponseStatus(HttpStatus.NO_CONTENT)
-	// void deleteAccount(@PathVariable(name = "user-id") Long userId, @Valid @RequestBody AccountDto account);
     
 }
