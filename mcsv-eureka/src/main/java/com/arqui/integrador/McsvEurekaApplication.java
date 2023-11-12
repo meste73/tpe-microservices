c23198dbefbf331669d56a95ca9493caa113e144
Ezequiel Mestelan <elmeste.88@gmail.com>
2023-11-12 16:26:33 -0300

    server eureka added

--- mcsv-eureka/src/main/java/com/arqui/integrador/McsvEurekaApplication.java
+++ mcsv-eureka/src/main/java/com/arqui/integrador/McsvEurekaApplication.java
@@ -2,14 +2,14 @@ package com.arqui.integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class McsvScooterApplication {
+@EnableEurekaServer
+public class McsvEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(McsvScooterApplication.class, args);
+		SpringApplication.run(McsvEurekaApplication.class, args);
 	}
 
 }
