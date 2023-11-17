c2c9648eff295a4519afb40bcfcf4817404b86b6
Ezequiel Mestelan <elmeste.88@gmail.com>
2023-11-17 18:23:57 -0300

    add path to avoid secutiry

--- mcsv-gateway/src/main/java/com/arqui/integrador/security/RouteValidator.java
+++ mcsv-gateway/src/main/java/com/arqui/integrador/security/RouteValidator.java
@@ -9,7 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class RouteValidator {
 	
-	public static final List<String> openApiEndpoints = List.of("/auth", "/swagger");
+	public static final List<String> openApiEndpoints = List.of(
+			"/auth", 
+			"/v2/api-docs",
+			"/v3/api-docs",
+			"/v3/api-docs/**",
+			"/swagger-resources",
+			"/swagger-resources/**",
+			"/configuration/ui",
+			"/configuration/security",
+			"/swagger-ui/**",
+			"/webjars/**",
+			"/swagger-ui.html");
 	
 	public Predicate<ServerHttpRequest> isSecured = 
 			request -> openApiEndpoints
